4fbb1ecdca19940b9339de5077fca4c40bb243ec
daduon <da.duon1997@gmail.com>
2020-06-29 09:15:46 +0700

    build app pizza

---
+++ app/Controllers/Pizza.php
@@ -0,0 +1,61 @@
+<?php namespace App\Controllers;
+use App\Models\PizzaModel;
+class Pizza extends BaseController
+{
+	// show list pizza
+	public function index()
+	{
+		$model = new PizzaModel();
+		$data['dataPizza'] = $model->findAll();
+		return view('index',$data);
+	}
+
+	// add new pizza
+	public function addPizza()
+	{	
+		helper(['form']);
+		$data = [];
+
+		if($this->request->getMethod() == "post"){
+			$rules = [
+				'name' => 'required',
+				'ingredients' => 'required',
+				'prize' => 'required'
+			];
+			if(!$this->validate($rules)){
+				$data['validate'] = $this->validator;
+				
+			}else{
+				$model = new PizzaModel();
+				$newData = [
+					'name' => $this->request->getVar('name'),
+					'ingredients' => $this->request->getVar('ingredients'),
+					'prize' => $this->request->getVar('prize')
+				];
+
+				$model->save($newData);
+				return redirect()->to('/dashboard');
+			}
+
+		}
+
+		// return view('index',$data);
+	}
+
+	// delete pizza
+	public function deletePizza($id)
+	{
+		$model = new PizzaModel();
+		$model->find($id);
+		$delete = $model->delete($id);
+		return redirect()->to('/dashboard');
+	}
+
+	// edit pizza
+	public function editPizza($id)
+	{
+		$model = new PizzaModel();
+		$data['edit'] = $model->find($id);
+		return view('index',$data);
+	}
+}
